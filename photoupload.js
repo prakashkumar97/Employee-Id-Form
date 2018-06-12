e3248beabdf06e95192003d1780702ede639bd93
prakash <prakashkb97@gmail.com>
2018-06-13 00:03:02 +0530

    All files updated

--- photoupload.js
+++ photoupload.js
@@ -1,194 +1,192 @@
 function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('#blah')
-                .attr('src', e.target.result)
-                // .width(248)
-                // .height(251);
-        };
-
-         reader.readAsDataURL(input.files[0]);
-    }
-}
-function addDetails()
-{
-    if (validateFirstName()) {
-        if (validateFullName()) {
-          if(employeecode())
-            {
-                if(validatemobile())
+    if (validateFirstName()) 
+    {
+        $('#errForFirstName').hide();
+        if (validateFullName()) 
+        {
+            $('#errForFullName').hide();
+            if(validateEmpCode())
                 {
-                 if(validateemergency())
+                    $("#errForEmpCode").hide();
+                    if(validatebloddGroup())
                     {
-                        if(validatemail())
+                        $("#errForBloodgrp").hide();
+                        if(validateEmail())
                         {
-                            if(validatebloodgroup())
-                                {
-                                   postData();
-                                }
-                                else
-                                    {
-                                        displayErrorMessageForBooldGroup();
-                                    }
-                        }
-                        else
+                            $("#errForEmail").hide();
+                            if(validateMobile())
                             {
-                                displayErrorMessageForMail();
+                                $("#errForMobile").hide();
+                                if(validateEmergencyMob())
+                                {
+                                    $("#errForEmergency").hide();
+                                    postData();
+                                }else{
+                                    displayErrorMessageForEmergency();
+                                }
+                            }else
+                            {
+                                displayErrorMessageForMobile();
                             }
-                    }
-                    else
-                        {
-                            displayErrorMessageForValidateemergency();
+                        }else{
+                            displayErrorMessageForMail();
                         }
-                }
-                else
-                    {
-                        displayErrorMessageForValidateMobile();
+                    }else{
+                        displayErrorMessageForBlood();
                     }
-            }
-           else
-            {
-                displayErrorMessageForEmployeeCode();
-            }
+                   // postData();
+                }else{
+                    displayErrorMessageForCode();
+                }
+            
         }
-        else {
+        else 
+        {
             // Display error message for full Name
             displayErrorMessageForFullName();
         }
-    } else {
+    } else 
+    {
         // Display error message for first Name
         displayErrorMessageForFirstName();
     }
 
 }
-
-function validatebloodgroup(){
- var blood=document.getElementById('bloodgroup');
-  var filter=/^([ABO\AB]) $/;
-  if (!filter.test(blood.value)) {
-    return false;
- }
- return true;
-
-}
-
-
-function  validatemobile(){
- const mobile=$('#mobile').val();
- if(mobile === ''){
-     return false;
- }
- return true;
-
-}
-
-function  validateemergency(){
-    const mobile=$('#emeregency').val();
-    if(mobile === ''){
-        return false;
-    }
-    return true;
-   
-   }
-   
-
-
-function employeecode()
+function displayErrorMessageForEmergency()
 {
-  const code=$('#empcode').val();
-  if(code===''){
-    return false;
-  }
-  return true;
+    $("#errForEmergency").show();
 }
-
-function validatemail()
+function displayErrorMessageForMobile()
 {
- 
-    var email = document.getElementById('mail');
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([virtusa])+\.)+([com])+$/;
-    
-    if (!filter.test(email.value)) {
-        return false;
-     }
-     return true;
-
+    $("#errForMobile").show();
 }
-function  displayErrorMessageForBooldGroup()
-{
- alert("Give the correct Blood group");
-
-}
-
 function displayErrorMessageForMail()
 {
- alert("Give the correct mail-id");
-
+    $("#errForEmail").show();
 }
-
-function displayErrorMessageForValidateemergency()
+function displayErrorMessageForBlood()
 {
- alert("Enter the emeregency contact number properly");
+    $("#errForBloodgrp").show();
+}
+function displayErrorMessageForCode()
+{
+    $("#errForEmpCode").show();
+}
+function displayErrorMessageForFirstName()
+ {
+    $('#errForFirstName').show();
 }
 
-function displayErrorMessageForValidateMobile(){
- alert("Enter the mobile number");
-
-}
-
-function displayErrorMessageForEmployeeCode(){
-alert("Enter the Employee code properly");
-
-}
-function displayErrorMessageForFirstName() {
-    alert("Enter the first name");
-}
-
-function displayErrorMessageForFullName() {
-    alert("Enter the full name");
+function displayErrorMessageForFullName()
+ {
+    $('#errForFullName').show();
 }
 
 
 function validateFirstName() {
-    const firstName = $('#inputFirstName').val();
-    if (firstName === '') {
+    const firstName = $('#eFirstName').val();
+    if (firstName === '' ) {
         return false;
-    }
+    }else if(/^[A-Za-z\s]+$/.test(firstName))
+    {
     return true;
+    }
+        
+    
 }
 
-function validateFullName() {
-    const fullName = $('#inputFullName').val();
+
+function validateFullName()
+ {
+    const fullName = $('#eFullName').val();
     if (fullName === '') {
         return false;
-    }
+    }else if(/^[A-Za-z\s]+$/.test(fullName))
+    {
     return true;
+    }
 }
+function validateEmpCode()
+{
+    var code = $('#eEmpcode').val();
+    
+    var ex = /^[0-9]+\.?[0-9]*$/;
+    if(code.length<6){
+    
+    if(ex.test(code) )
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
+       
+    }else{
+        return false;
+    }
+}
+function validateEmail()
+{
+    const mail=$("#mailid").val();
+    var reg= /@virtusa.com\s*$/;
+    if(reg.test(mail))
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+function validateMobile()
+{
+const mob=$("#mobile").val();
+var exp=/^(\+\d{1,3}[- ]?)?\d{10}$/;
+if(exp.test(mob))
+{
+    return true;
+}else{
+    return false;
+}
+}
+function validateEmergencyMob()
+{
+    const mob1=$("#emergency").val();
+    var exp1=/^(\+\d{1,3}[- ]?)?\d{10}$/;
+    if(exp1.test(mob1))
+    {
+        return true;
+    }else{
+        return false;
+    }
+ }
 
-function postData() {
+function postData() 
+{
     const data = {
         firstName: $('#eFirstName').val(),
         fullName: $('#eFullName').val(),
-        Designation: $('#eDesignation').val(),
-        EmployeeCode: $('#eEmpcode').val(),
-        BloodGroup: $('#eBloodGroup').val(),
-        Reason: $('eReason').val(),
-        EmployeeDate: $('#empdate').val(),
-        MailID: $('#mailid').val(),
-        MobileNumber: $('#mobile').val(),
-        EmergencyNumber: $('#emergency').val()
+        Designtion:$('#eDesignation').val(),
+        EmployeeCode:$('#eEmpcode').val(),
+        BloodGrp:$('#eBloodGroup').val(),
+        ReasonsForIssue:$('#eReason').val(),
+        DateofEmployment:$('#empdate').val(),
+        Email:$('#mailid').val(),
+        MobileNo:$('#mobile').val(),
+        EmergencyMobile:$('#emergency').val()
     };
     $.ajax({
         type: "POST",
-        url: 'https://application-67c67.firebaseio.com/.json',
+        url: 'https://employeeid-6bd95.firebaseio.com/Employee.json',
         data: JSON.stringify(data),
         success: onPostSuccess,
-        // dataType: dataType
     });
 }
 
 const onPostSuccess = (data) => {
-    alert("Form Submitted Successfully");
+    console.log('Posting to firebase success');
+    console.log(data);
 }
+
+$('document').ready(() => {
+    $('.span-for-errors').hide();
+});
